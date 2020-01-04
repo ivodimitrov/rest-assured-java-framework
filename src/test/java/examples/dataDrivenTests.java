e0c375faf6c3e62b59ddbf45bce7a0ee5b7eccee
IvoDimitrov <ivopdimitrov@gmail.com>
2020-01-04 18:17:13 +0200

    Added dataDrivenTests.java

---
+++ src/test/java/examples/dataDrivenTests.java
@@ -0,0 +1,36 @@
+package examples;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(DataProviderRunner.class)
+public class dataDrivenTests {
+
+    @DataProvider
+    public static Object[][] zipCodesAndPlaces() {
+        return new Object[][]{
+                {"us", "90210", "Beverly Hills"},
+                {"us", "12345", "Schenectady"},
+                {"ca", "B2R", "Waverley"}
+        };
+    }
+
+    @Test
+    @UseDataProvider("zipCodesAndPlaces")
+    public void requestZipCodesFromCollection_checkPlaceNameInResponseBody_expectSpecifiedPlaceName(String countryCode, String zipCode, String expectedPlaceName) {
+
+        given().
+                pathParam("countryCode", countryCode).pathParam("zipCode", zipCode).
+                when().
+                get("http://zippopotam.us/{countryCode}/{zipCode}").
+                then().
+                assertThat().
+                body("places[0].'place name'", equalTo(expectedPlaceName));
+    }
+}
